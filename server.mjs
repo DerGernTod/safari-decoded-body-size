1eab77d5c5f5bbb2a2a46048b5175b2e69a5eda8
Gernot Raudner <gernot.raudner@dynatrace.com>
2024-07-18 12:22:41 +0200

    reproducer server

---
+++ server.mjs
@@ -0,0 +1,12 @@
+import http from 'http';
+import fs from 'fs/promises';
+
+const port = 3000;
+
+http.createServer(async (req, res) => {
+    const data = await fs.readFile('index.html', 'utf8');
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(data);
+}).listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
